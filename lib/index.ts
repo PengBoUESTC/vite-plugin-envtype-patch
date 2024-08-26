342f75ec909c8e3da9d257ee206c2dfc644312c1
pengbo43 <pengbo43@jd.com>
2024-08-26 18:40:09 +0800

    chore: apply

--- lib/index.ts
+++ lib/index.ts
@@ -2,7 +2,7 @@ import { writeFile } from 'node:fs'
 import { join } from 'node:path'
 import { cwd } from 'node:process'
 
-import { type ResolvedConfig } from 'vite'
+import { type ResolvedConfig, type Plugin } from 'vite'
 // @ts-ignore
 import { generateIdentifierDeclarationFile } from 'dts-gen'
 
@@ -11,12 +11,13 @@ export interface PatchConfig {
   fileName: string
 }
 
-export const envTypePatch = (config?: Partial<PatchConfig>) => {
+export const envTypePatch = (config?: Partial<PatchConfig>): Plugin => {
   const { patchKey = 'ImportMetaEnv', fileName = 'env.d.ts' } = config || {}
   const targetPath = join(cwd(), fileName)
 
   return  {
     name: 'vite-plugin-envtype-patch',
+    apply: 'serve',
     configResolved(resolvedConfig: ResolvedConfig) {
       const { env } = resolvedConfig
       const envDts = generateIdentifierDeclarationFile(patchKey, env)
